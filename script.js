b5de6cbb34021f8b5894b42489cfae6b99fe03a5
Evan <j.evan.woods@gmail.com>
2023-02-17 09:22:44 -0500

    Added basic script to connect to input variables.

---
+++ script.js
@@ -0,0 +1,17 @@
+let rawNTU = document.getElementById('rawNTU').value;
+let rawpH = document.getElementById('rawpH').value;
+let rawTemp = document.getElementById('rawTemp').value;
+let rawAlk =document.getElementById('rawAlk').value;
+
+console.log("hello world");
+
+function coagulantCalc() {
+    rawNTU = document.getElementById('rawNTU').value;
+    rawpH = document.getElementById('rawpH').value;
+    rawTemp = document.getElementById('rawTemp').value;
+    rawAlk =document.getElementById('rawAlk').value;
+    console.log("Raw NTU is: " + rawNTU);
+    console.log("Raw pH is: " + rawpH);
+    console.log("Raw pH is: " + rawTemp);
+    console.log("Raw pH is: " + rawAlk);
+}
